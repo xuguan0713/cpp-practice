c760ad683d4d1dd5aeb6272cfeace2f3fabea20c
xuguan0713 <xuguan0713@gmail.com>
2026-04-16 02:01:48 +0900

    Add files via upload

---
+++ Penguin.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    std::cout << "  -----------------\n";
+    std::cout << " |                 |\n";
+    std::cout << " |     MESSAGE     |\n";
+    std::cout << " |       HERE      |\n";
+    std::cout << " |                 |\n";
+    std::cout << "  -----------------\n";
+    std::cout << "      \\\\   //\n";
+    std::cout << "       (o o)\n";
+    std::cout << "      /  V  \\\\\n";
+    std::cout << "     /(  _  )\\\\\n";
+    std::cout << "       ^^ ^^\n";
+}
---
+++ Tom20.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string name = "Tom";
+    int age = 20;
+
+    std::cout << name << "\n";
+    std::cout << age << "\n";
+}
